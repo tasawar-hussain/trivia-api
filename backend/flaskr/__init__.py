b946b76e8ecb56a67831fcfab7365f189cfb7e79
Tasawar Hussain <31658686+tasawar-hussain@users.noreply.github.com>
2021-10-23 23:40:05 +0500

    Implement question endpoint with pagination

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,6 +9,20 @@ from models import setup_db, Question, Category
 QUESTIONS_PER_PAGE = 10
 
 
+def paginate_questions(request, selection):
+    """
+    helper function for pagination
+    """
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    books = [book.format() for book in selection]
+    current_books = books[start:end]
+
+    return current_books
+
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -30,18 +44,32 @@ def create_app(test_config=None):
     Create an endpoint to handle GET requests
     for all available categories.
     '''
-    '''
-    @TODO:
-    Create an endpoint to handle GET requests for questions,
-    including pagination (every 10 questions).
-    This endpoint should return a list of questions,
-    number of total questions, current category, categories.
 
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions.
-    '''
+    @app.route("/questions")
+    def retrieve_questions():
+        """
+        Create an endpoint to handle GET requests for questions,
+        including pagination (every 10 questions).
+        This endpoint should return a list of questions,
+        number of total questions, current category, categories.
+        """
+
+        selection = Question.query.order_by(Question.id).all()
+        current_questions = paginate_questions(request, selection)
+        categories = Category.query.order_by(Category.type).all()
+        categories = {category.id: category.type for category in categories}
+
+        if len(current_questions) == 0:
+            abort(404)
+
+        return jsonify({
+            "success": True,
+            "questions": current_questions,
+            "total_questions": len(selection),
+            "categories": categories,
+            "current_category": None
+        })
+
     '''
     @TODO:
     Create an endpoint to DELETE question using a question ID.
